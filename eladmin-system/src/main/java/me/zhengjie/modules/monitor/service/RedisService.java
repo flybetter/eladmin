3f1959ac6dbe88ffdb82ae3d75cb1663cd7595cc
flybetter <flybetter@163.com>
2019-06-05 15:56:53 +0800

    init commit

---
+++ eladmin-system/src/main/java/me/zhengjie/modules/monitor/service/RedisService.java
@@ -0,0 +1,31 @@
+package me.zhengjie.modules.monitor.service;
+
+import me.zhengjie.modules.monitor.domain.vo.RedisVo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+/**
+ * 可自行扩展
+ * @author jie
+ * @date 2018-12-10
+ */
+public interface RedisService {
+
+    /**
+     * findById
+     * @param key
+     * @return
+     */
+    public Page findByKey(String key, Pageable pageable);
+
+    /**
+     * delete
+     * @param key
+     */
+    public void delete(String key);
+
+    /**
+     * 清空所有缓存
+     */
+    public void flushdb();
+}
